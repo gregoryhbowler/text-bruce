7f05281ba6dd0be37acd89254b05d7ab9266936e
gregoryhbowler <greg@brudmusic.com>
2015-09-02 13:46:06 -0700

    bruce to user text

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -10,7 +10,8 @@ class SmsController < ApplicationController
   def reply
     puts params
     puts "here is the body #{params[:Body]}"
-    if User.where(phone_number: params[:From]).count ==0
+    u = User.where(phone_number: params[:From]).first
+    if !u
       twilio_client.messages.create(
         to: params[:From],
         from: ENV['TWILIO_PHONE_NUMBER'],
@@ -19,13 +20,24 @@ class SmsController < ApplicationController
       return
     end
     if params[:From] == "+13233016764"
+      name = params[:Body].slice(0..(str.index('-')-1))
+      if User.where(name: name).first
 
+        twilio_client.messages.create(
+          to: u.phone_number,
+          from: ENV['TWILIO_PHONE_NUMBER'],
+          body: params[:Body].slice(str.index('-')..str.length-1)
+        )
+      else
+
+      end
     else
       twilio_client.messages.create(
         to: "+13233016764",
         from: ENV['TWILIO_PHONE_NUMBER'],
         body: params[:Body]
       )
+      Message.create(body: params[:Body], user: u)
     end
     respond_to do |format|
       format.json { render :status => 200 }
